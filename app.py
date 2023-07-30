d81e37899ffe3d78a71124a7c475316c14549838
ashishcs10dec <ashishsharansrivastava@gmail.com>
2023-07-30 18:41:14 +0530

    second commit

--- app.py
+++ app.py
@@ -1,17 +1,54 @@
-from flask import Flask, render_template, request,jsonify
+from flask import Flask, render_template, request,jsonify,url_for
 import requests
 from bs4 import BeautifulSoup
 from urllib.request import urlopen as uReq
 import logging
-logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
 import os
+import csv
+logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
 
 application=Flask(__name__)
 app=application
 
-@app.route("/", methods = ['GET'])
+@app.route("/", methods = ['POST','GET'])
 def index():
-    return render_template("index.html")
+    is_failure=False
+    message=''
+    if request.method == 'POST':
+        try:
+             url=request.form['item_url'].replace(" ","")
+             headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
+             #url=f"https://www.flipkart.com/godrej-180-l-direct-cool-single-door-3-star-refrigerator/product-reviews/itm90f2ef108b3d3?pid=RFRGZNRAW8MFVHDQ&lid=LSTRFRGZNRAW8MFVHDQMPJG6Q&marketplace=FLIPKART"
+             response = requests.get(url)
+             soup = BeautifulSoup(response.content, "html.parser")
+
+             title=soup.find_all("p",{"class":"_2-N8zT"})
+             rating=soup.find_all("div",{"class":"_3LWZlK _1BLPMq"})
+             review=soup.find_all("div",{"class":"t-ZTKy"})
+
+             # field names 
+             fields = ['Title', 'Rating', 'Review']
+
+             rows=[]
+             for i in range(len(title)):
+                row={'Title':title[i].getText(),'Rating':rating[i].getText(),'Review':review[i].getText()}
+                rows.append(row)
+
+             filename='flipkart_item_scraping.csv'
+             with open(filename, 'w',encoding='utf-8') as csvfile:
+              # creating a csv dict writer object 
+              writer = csv.DictWriter(csvfile, fieldnames = fields)
+              # writing headers (field names) 
+              writer.writeheader()
+              # writing data rows 
+              writer.writerows(rows)
+              message='Data pulled successfully!'
+
+        except Exception as e:
+                    logging.info(e)
+                    #return 'OOPs! Something is wrong.'
+                    is_failure=True
+    return render_template("index.html",flag=is_failure,msg=message)
 
 if __name__ == "__main__":
     app.run()
